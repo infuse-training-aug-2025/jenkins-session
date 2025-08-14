5751105461f01b04c63961a56487e121d708fbd8
Ekata Nabar <ekatanabar@gmail.com>
2025-08-14 15:30:33 +0530

    cs

--- trial.bat
+++ trial.bat
@@ -3,7 +3,7 @@ set /a num=%random%%%60+1
 echo Generated number: %num%
 if %num% lss 30 (
     echo Script failed with number %num%
-    exit /b 2
+    exit /b 1
 ) else (
     echo Script succeeded with number %num%
     exit /b 0
